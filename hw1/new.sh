019db7c670915e9a46aea4f85c18b0666f30c7d8
itzhakMan <itzhak.man@campus.technion.ac.il>
2021-04-07 21:58:08 +0300

    add new.sh

--- hw1/new.sh
+++ hw1/new.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget  https://www.premierleague.com/players
-grep -P "/players/([0-9]+)/(([a-zA-Z]+)|([a-zA-Z]+)-([a-zA-Z]+))/overview" players
+grep -P "/players/([0-9]+)/(([a-zA-Z]+)|([a-zA-Z]+)-([a-zA-Z]+))/overview" players >url.txt
