31563ee3db2eb747e3ce47954227ff811c14ed20
itzhakMan <itzhak.man@campus.technion.ac.il>
2021-04-08 19:45:59 +0300

    add hw1

--- hw1/new.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget  https://www.premierleague.com/players
-grep -P "/players/([0-9]+)/(([a-zA-Z]+)|([a-zA-Z]+)-([a-zA-Z]+))/overview" players >url.txt
---
+++ hw1/scrape_players.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -f url.txt
+wget -q https://www.premierleague.com/players
+grep -oP "/players/([0-9]+)/(([a-zA-Z]+)|([a-zA-Z]+)-([a-zA-Z]+))/overview" players > url.txt
+total=$(grep -oP "/players/([0-9]+)/(([a-zA-Z]+)|([a-zA-Z]+)-([a-zA-Z]+))/overview" players | wc -l) 
+echo Total Players: $total >> results.csv
+sed -i 's/\/players/https:\/\/www\.premierleague\.com\/players/' url.txt
+while read -r line
+do
+	x=$(wget -qO- $line | grep -P "([mM]idfielder)+" | wc -w)
+	if [ $x -gt 0 ]
+	then
+		echo $line, Midfielder, $x >> results.csv
+	fi
+done < url.txt
