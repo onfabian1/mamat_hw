f4e8ed74e5b88faef7847ffdad42593c19459db6
itzhakMan <itzhak.man@campus.technion.ac.il>
2021-04-12 22:44:09 +0300

    add scrape_players.sh

--- hw1/scrape_players.sh
+++ hw1/scrape_players.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -f url.txt
 wget -q https://www.premierleague.com/players
-grep -oP "/players/([0-9]+)/(([a-zA-Z]+)|([a-zA-Z]+)-([a-zA-Z]+))/overview" players > url.txt
+grep -oP "/players/([0-9]+)/(([a-zA-Z-]+))/overview" players > url.txt
 total=$(grep -oP "/players/([0-9]+)/(([a-zA-Z-]+))/overview" players | wc -l) 
 echo Total Players: $total >> results.csv
 sed -i 's/\/players/https:\/\/www\.premierleague\.com\/players/' url.txt
