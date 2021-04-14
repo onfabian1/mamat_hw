520a5274db6de71524530cef4f6ab01fc206efed
On Fabian <onfabian@campus.technion.ac.il>
2021-04-14 13:16:20 +0300

    part 1

---
+++ hw2/course_stat.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+chmod +x course_stat.sh
+
+#check valid num of args
+
+if [ 1 != $# ]
+then 
+	echo "Wrong number of arguments" >&2
+	exit 
+fi
+
+#check xxxxxx.txt exsits
+
+var=$(ls -a | grep $1.txt)
+if [[ $var == '' ]] 
+then
+	echo "Course Not Found" >&2
+	exit
+fi
+
+#check folder xxxxxx_stat
+
+var=$(ls -a | grep $1_stat)
+if [[ $var != '' ]]
+then
+	rm -r $1_stat
+fi
+mkdir $1_stat
+
+#writing to grades.txt
+
+cat $1.txt | tail -n +2 | awk '{print $2}' > $1_stat/grades.txt
+cd $1_stat
+cat grades.txt
+
+
+
