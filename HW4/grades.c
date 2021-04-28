fda3b51e89cb6c0faf69379a5ace4a394d7abb9a
On Fabian <onfabian@campus.technion.ac.il>
2021-04-28 21:29:41 +0300

    good progress

--- HW4/grades.c
+++ HW4/grades.c
@@ -1,13 +1,27 @@
 typedef struct student{
     char* name;
     int id;
-} * student_t
+    struct list* courses;
+} *student_t
+
+typedef struct course{
+	char* course_name;
+	double grade;
+} *course_t
+
 
 int cloneStudent(void* student, void **out){
     student_t clone = (student_t) malloc(sizeof(struct student));
+    //TODO add malloc check
     clone->id = (student_t)student->id;
     clone->name = strcpy((student_t)student->name);
+    
+    struct iterator it = list_begin(student->courses);
+    for(; it!=list_end(student->courses);it=list_next(student->courses)) {
+    	list_push_back(clone->courses, it);
+    }
     *out = clone;
+    return 0;
 }
 
 void destroyStudent(void * student){
@@ -16,13 +30,14 @@ void destroyStudent(void * student){
 }
 
 
+
 typedef struct grades{
-    list* 
+     list students_list;
 } *grades_t
 
 grades_t grades_init(){
-    grades_t g = (grades_t)new malloc(sizeof(struct grades));
-    grades_t->userList = list_init(&cloneStudent, &destroyStudent);
+    grades_t grades_sys = (grades_t)malloc(sizeof(struct grades));
+    grades_t->students_list = list_init(&cloneStudent, &destroyStudent);
 }
 void grades_destroy(struct grades *grades){
     list_destroy(grade->studentList);
