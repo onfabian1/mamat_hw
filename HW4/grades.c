18e94b36b0d44e4b95e36113bf5e0eae03025042
on <onfabian@campus.technion.ac.il>
2021-04-30 14:33:29 +0300

    work on funcs

--- HW4/grades.c
+++ HW4/grades.c
@@ -23,15 +23,48 @@ void grades_destroy(struct grades *grades){
     free(grades_t);
 }
 
+int grades_add_student(grades_t, const char *name, int id){
+	struct iterator* it = list_begin(grades_t->students_list);
+	for(;it!=list_end(grades_t->students_list);it=list_next(grades_t->students_list){
+		if(id==it->id){
+			printf("Error");
+			return 1;
+		}
+	}
+	list_push_back(grades_t->students_list, student_t);
+	list_end(grades_t->students_list)->name=strcpy(name);
+	list_end(grades_t->students_list)->id=id;
+	student_t->courses = list_init(&clone_course, &destroy_course);                 
+	return 0;
+}
+	
+int grades_add_grade(grades_t,const char *name, int id, int grade){
+	//checks if student.id exists
+	int result=0;
+	struct iterator* it = list_begin(grades_t->students_list);
+	for(;it!=list_end(grades_t->students_list);it=list_next(grades_t->students_list){
+		if(it->id==id){
+			result ++;
+			break;
+		}
+	}
+    if(!result){
+    	printf("Error");
+    	return 1;
+    }               
+                   
+                    
+
 int clone_student(void* student, void **out){
     student_t clone = (student_t) malloc(sizeof(struct student));
-    //todo add malloc check
-    clone->id = (student_t)student->id; //maybe student_t->id;
-    clone->name = strcpy((student_t)student->name);
+    //TODO add malloc check
+    clone->id = student_t->id; 
+    clone->name = strcpy(student_t->name);
     
-    struct iterator it = list_begin(student->courses);
-    for(; it!=list_end(student->courses);it=list_next(student->courses)) {
-    	list_push_back(clone->courses, it);
+    struct iterator* it = list_begin(student_t->courses);
+    for(; it!=list_end(student_t->courses);it=list_next(student_t->courses)) {
+    	if(list_push_back(clone->courses, it)){
+    		printf("ERROR");
     }
     *out = clone;
     return 0;
@@ -39,15 +72,20 @@ int clone_student(void* student, void **out){
 
 void destroy_student(void * student){
     free((student_t)student->name);
-    free(student);
-    //todo free courses by linked-list.h funcs
+    list_destroy(student_t->courses);
+    free(student_t);
 }
 
 int clone_course(void* course, void **out){
 	course_t clone = (course_t) malloc(sizeof(struct course));
 	//todo if malloc
-	clone->grade = (course_t)course->grade;
-	clone->name = strcpy((course_t)course->name);
+	clone->grade = course_t->grade;
+	clone->name = strcpy(course_t->name);
 	*out=clone;
 	return 0;
 }
+
+void destroy_course(void *course){
+	free(course_t->name);
+	free(course_t);
+	}
