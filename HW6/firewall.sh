ed029a0dbff3b360c0006ce46ccca988143255f3
on <onfabian@campus.technion.ac.il>
2021-05-29 23:28:54 +0300

    final

--- HW6/firewall.sh
+++ HW6/firewall.sh
@@ -6,24 +6,24 @@ firewall_pkts=""
 
 while read rule
 do
-	
-	rule=`echo "$rule" | tr -d ' ' | sed '/^$/d' | grep -o '^[^#]*'`
-	#rule=`echo "$rule" | tr -d ' ' | grep -o '^[^#]*'` | sed -r 's/[,]+/,/g' | sed -r 's/^[,]//g'    #grep -o '^[a-z]' | grep -o '$[0-9]' 
-	if [[ -n $rule ]]; then
+	match_pkts=""
+	#rule=`echo "$rule" | sed 's/ //g' | sed '/^$/d' | grep -o '^[^#]*'`
+	rule=`echo "$rule" | tr -d ' ' | sed '/^$/d' | grep -o '^[^#]*' | sed -r 's/'[,]+'/,/g' | sed -r 's/'^[,]'//g'`  
+	if [[ "$rule" == "" ]]; then
+		continue
+	fi
+
 	IFS=',' read -ra arr <<< "$rule"
+		#	echo "$packets" | valgrind --leak-check=full ./firewall.exe "${arr[0]}" 2>&1 
+		  # echo "$packets" | valgrind --leak-check=full ./firewall.exe "${arr[1]}" 2>&1 
+		  #  echo "$packets" | valgrind --leak-check=full ./firewall.exe "${arr[2]}" 2>&1 
+		  #  echo "$packets" | valgrind --leak-check=full ./firewall.exe "${arr[3]}" 2>&1 
 	match_pkts=`echo "$packets" | ./firewall.exe "${arr[0]}" | ./firewall.exe "${arr[1]}"` 
 	match_pkts=`echo "$match_pkts"| ./firewall.exe "${arr[2]}" | ./firewall.exe "${arr[3]}"`
-	#for filter in "${rule_array[@]}"
-	#do
-		#echo "$packets" | valgrind --leak-check=full ./firewall.exe "$filter" 2>&1 | grep ERROR
-		#match_pkts=echo "$packets"
-    	#match_pkts=`./firewall.exe "$filter" < "$match_pkts"`
-	#done
-	firewall_pkts+=`echo -e "${match_pkts}" |  tr -d ' ' | sort -u`
+	firewall_pkts+=`echo -e "${match_pkts}" | sed '/^[[:space:]]*$/d' | sort -u ` #| uniq -c  | grep -E "^ *${#arr[@]} " | sed -e "s/^ *${#rarr[@]} //"`
 	firewall_pkts+="\n"
-	fi
-	
-done < "$1"
+
+done <"$1"
 
 
-echo -e "${firewall_pkts}" | sed '/^[[:space:]]*$/d' | tr -d ' ' | sort -u
+echo -e "${firewall_pkts}" | sed '/^$/d' | tr -d ' ' | sort -u
